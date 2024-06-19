1213ef06e42dedbab03bdd06f9bf876c8cbad2bb
Stephen Afari <demorganafari@yahoo.com>
2024-06-19 21:49:28 +0000

    date picker

--- src/routes/budget/myBudget.js
+++ src/routes/budget/myBudget.js
@@ -1,6 +1,9 @@
 import { SplitScreen } from "../../components/splitScreen/splitScreen";
 import { IoReorderFourSharp } from "react-icons/io5";
 import { BudgetHeader, BudgetIconContainer} from "./myBudget.styles";
+import { useState } from "react";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const MiddleComponent=({name})=>{
     return(<p style={{backgroundColor: 'yellow'}}>{name}</p>)
@@ -11,9 +14,14 @@ const MiddleComponent=({name})=>{
   }
   
   export const MyBudgetScreen=()=>{
+    const [date, setDate]= useState(new Date())
     return(
       <>
      <BudgetHeader><BudgetIconContainer><IoReorderFourSharp /> </BudgetIconContainer>Budget</BudgetHeader>
+     {/* //Date Picker component */}
+      <div>
+      <DatePicker selected={date} onChange={(date)=>setDate(date)}  dateFormat="MMMM yyyy"
+          placeholderText="Select a date"/></div>
       <SplitScreen  middleWeight={1} rightWeight={3}>
       <MiddleComponent name="Middle"/>
       <RightComponent name1="Right"/>
