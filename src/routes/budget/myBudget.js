27475c73f53879093ba9cc3bc1176bcf6cb388ef
Stephen Afari <demorganafari@yahoo.com>
2024-06-24 05:06:53 +0000

    budget page

--- src/routes/budget/myBudget.js
+++ src/routes/budget/myBudget.js
@@ -1,29 +1,94 @@
 import { SplitScreen } from "../../components/splitScreen/splitScreen";
 import { IoReorderFourSharp } from "react-icons/io5";
-import { BudgetHeader, BudgetIconContainer} from "./myBudget.styles";
+import { BudgetHeader, GlobalStyle,BudgetIconContainer, MyMiddleComponent,Table, TableHead,TableRow,TableData,TableContainer, DatePickerContainer} from "./myBudget.styles";
 import { useState } from "react";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+//Date format MM/dd/yy
+const transactions=[
+  {id: 1, date: '06-10-2024', subGroup:'water', parent:'utilities', description:'water bill',amount: 70},
+  {id: 2, date: '05-10-2024', subGroup:'electricity', parent:'utilities',description:'light bill' ,amount: 700},
+  {id: 3, date: '04-10-2024', subGroup:'maintenance', parent:'housing', description:'window frame' ,amount: 200},
+  {id: 4, date: '03-10-2024', subGroup:'fees', parent:'childcare & education', description:'Afia fees' ,amount: 200},
+]
 
-const MiddleComponent=({name})=>{
-    return(<p style={{backgroundColor: 'yellow'}}>{name}</p>)
-  }
+const incomes=[
+  {id: 1, date: '06-10-2024', subGroup:'paycheck', parent:'salary', description:'person 1',amount: 70},
+  {id: 2, date: '05-10-2024', subGroup:'paycheck', parent:'salary',description:'person 2' ,amount: 700},
+  {id: 3, date: '04-10-2024', subGroup:'paycheck', parent:'bonus', description:'person 1' ,amount: 200},
+  {id: 4, date: '03-10-2024', subGroup:'paycheck', parent:'bonus', description:'person 2' ,amount: 200},
+  {id: 4, date: '03-10-2024', subGroup:'windfall', parent:'miscellaneous', description:'windfall' ,amount: 200},
+]
+
+// const MiddleComponent=()=>{
+//     return(<MyMiddleComponent></MyMiddleComponent>)
+//   }
   
   const RightComponent=({name1})=>{
     return(<p style={{backgroundColor: 'green'}}>{name1}</p>)
   }
   
   export const MyBudgetScreen=()=>{
-    const [date, setDate]= useState(new Date())
+  
+    const [selectedDate, setSelectedDate]= useState(null);
+    //filtering to show data for the selected date only 
+    const filteredTransactions = selectedDate ?
+      transactions.filter((transaction)=>{
+        return new Date(transaction.date).toString()===selectedDate.toString();
+       
+      }):[];
+     //filtering to show data for the selected date only 
+     const filteredIncome = selectedDate ? (
+      incomes.filter((income)=>{
+       return new Date(income.date).toString()=== selectedDate.toString();
+      })
+     ):[];
     return(
       <>
      <BudgetHeader><BudgetIconContainer><IoReorderFourSharp /> </BudgetIconContainer>Budget</BudgetHeader>
      {/* //Date Picker component */}
-      <div>
-      <DatePicker selected={date} onChange={(date)=>setDate(date)}  dateFormat="MMMM yyyy"
-          placeholderText="Select a date"/></div>
+      <DatePickerContainer>
+        <GlobalStyle/>
+      <DatePicker selected={selectedDate} onChange={(selectedDate)=>setSelectedDate(selectedDate)}  dateFormat="MMMM-d-yyyy"
+          placeholderText="Select a date"/></DatePickerContainer>
       <SplitScreen  middleWeight={1} rightWeight={3}>
-      <MiddleComponent name="Middle"/>
+      <MyMiddleComponent >
+<TableContainer>
+      <Table>
+        <TableRow>
+            <TableHead>Income</TableHead>
+            
+            <TableHead>Amount</TableHead>
+        </TableRow>
+      {filteredIncome.length >0 ? (
+      filteredIncome.map((income)=>(
+       <TableRow key={income.id}>
+       <TableData>{income.parent}</TableData>
+      
+       <TableData>{income.amount}</TableData>
+        </TableRow>
+   ))):(<div>No Transaction selected for this date</div>)}
+   </Table>
+</TableContainer>
+
+<TableContainer>
+      <Table>
+        <TableRow>
+            <TableHead>Income</TableHead>
+            
+            <TableHead>Amount</TableHead>
+        </TableRow>
+      {filteredTransactions.length >0 ?
+      (filteredTransactions.map((transaction)=>(
+        <TableRow key={transaction.id}>
+       <TableData>{transaction.parent}</TableData>
+      
+       <TableData>{transaction.amount}</TableData>
+        </TableRow>
+      ))):(<div>No Transaction selected for this date</div>)}
+    </Table>
+    </TableContainer>
+      </MyMiddleComponent>
       <RightComponent name1="Right"/>
         </SplitScreen>
         </>
--- src/routes/budget/myBudget.styles.jsx
+++ src/routes/budget/myBudget.styles.jsx
@@ -1,9 +1,73 @@
-import styled from 'styled-components';
+import styled,{ createGlobalStyle }  from 'styled-components';
+import 'react-datepicker/dist/react-datepicker.css';
+
+//createGlobalStyle from styled-components is used to define global styles that target the react-datepicker input elements.
+export const GlobalStyle= createGlobalStyle`
+.react-datepicker__input-container input {
+    border: none;
+    box-shadow: none;
+  }
+
+  .react-datepicker__input-container input:focus {
+    outline: none;
+  }
+`
 
 export const BudgetHeader = styled.h4`
 display: flex;
 margin-top: 2px;
+margin-left: 5px
 `
 export const BudgetIconContainer = styled.div`
 margin-top: 2px;
+`
+
+export const MyMiddleComponent = styled.div`
+background-color: #EEEEEE;
+width: 100%;
+height: 540px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+
+`
+export const Table = styled.table`
+width:400px;
+margin-left:20px;
+margin-right:2px;
+margin-top: 4%
+
+`
+export const DatePickerContainer=styled.div`
+margin-left:6px;
+width: 
+`
+export const TableRow = styled.tr`
+display: flex;
+justify-content: space-between;
+&:first-child {
+    background-color:#EEF7FF;
+}
+
+`
+
+export const TableHead = styled.th`
+
+color: #686D76;
+  text-align: left;
+`
+export const TableData = styled.td`
+
+`
+export const TableContainer=styled.div`
+width:500px;
+// max-width: 200px;
+overflow-x: auto;
+border-radius: 8px;
+padding: 20px;
+ background-color:white;
+height:200px;
+margin-top:30px;
+
+
 `
