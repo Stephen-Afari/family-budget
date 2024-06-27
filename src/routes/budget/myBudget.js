4d55d2492cc134ca8a9819c9642095107f18f58b
Stephen Afari <demorganafari@yahoo.com>
2024-06-27 20:07:43 +0000

    income and exp. tables

--- src/routes/budget/myBudget.js
+++ src/routes/budget/myBudget.js
@@ -1,23 +1,30 @@
 import { SplitScreen } from "../../components/splitScreen/splitScreen";
 import { IoReorderFourSharp } from "react-icons/io5";
-import { BudgetHeader, GlobalStyle,BudgetIconContainer, MyMiddleComponent,Table, TableHead,TableRow,TableData,TableContainer, DatePickerContainer} from "./myBudget.styles";
+import { BudgetHeader,RemoveSymbol,HorizontalRule,AddSymbol, GlobalStyle,BudgetIconContainer, MyMiddleComponent,Table, TableHead,TableRow,TableData,TableContainer, DatePickerContainer} from "./myBudget.styles";
 import { useState } from "react";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+
+
+//// Utility function for currency formatting
+const formatCurrency = (value, locale = 'en-GH', currency = 'GHS') => {
+  return new Intl.NumberFormat(locale, { style: 'currency', currency }).format(value);
+};
+
 //Date format MM/dd/yy
 const transactions=[
-  {id: 1, date: '06-10-2024', subGroup:'water', parent:'utilities', description:'water bill',amount: 70},
-  {id: 2, date: '05-10-2024', subGroup:'electricity', parent:'utilities',description:'light bill' ,amount: 700},
-  {id: 3, date: '04-10-2024', subGroup:'maintenance', parent:'housing', description:'window frame' ,amount: 200},
-  {id: 4, date: '03-10-2024', subGroup:'fees', parent:'childcare & education', description:'Afia fees' ,amount: 200},
+  {id: 1, date: '06-10-2024', subGroup:'water', parent:'utilities', description:'water bill',amount: 70, target:100},
+  {id: 2, date: '06-10-2024', subGroup:'electricity', parent:'utilities',description:'light bill' ,amount: 700, target:100},
+  {id: 3, date: '06-10-2024', subGroup:'maintenance', parent:'housing', description:'window frame' ,amount: 200, target:100},
+  {id: 4, date: '03-10-2024', subGroup:'fees', parent:'childcare & education', description:'Afia fees' ,amount: 200, target:100},
 ]
 
 const incomes=[
-  {id: 1, date: '06-10-2024', subGroup:'paycheck', parent:'salary', description:'person 1',amount: 70},
-  {id: 2, date: '05-10-2024', subGroup:'paycheck', parent:'salary',description:'person 2' ,amount: 700},
-  {id: 3, date: '04-10-2024', subGroup:'paycheck', parent:'bonus', description:'person 1' ,amount: 200},
-  {id: 4, date: '03-10-2024', subGroup:'paycheck', parent:'bonus', description:'person 2' ,amount: 200},
-  {id: 4, date: '03-10-2024', subGroup:'windfall', parent:'miscellaneous', description:'windfall' ,amount: 200},
+  {id: 1, date: '06-10-2024', subGroup:'paycheck', parent:'salary', description:'person 1',amount: 70, target:200},
+  {id: 2, date: '05-10-2024', subGroup:'paycheck', parent:'salary',description:'person 2' ,amount: 700, target:200},
+  {id: 3, date: '04-10-2024', subGroup:'paycheck', parent:'bonus', description:'person 1' ,amount: 200, target:200},
+  {id: 4, date: '03-10-2024', subGroup:'paycheck', parent:'bonus', description:'person 2' ,amount: 200, target:200},
+  {id: 4, date: '03-10-2024', subGroup:'windfall', parent:'miscellaneous', description:'windfall' ,amount: 200, target:200},
 ]
 
 // const MiddleComponent=()=>{
@@ -58,35 +65,49 @@ const incomes=[
         <TableRow>
             <TableHead>Income</TableHead>
             
-            <TableHead>Amount</TableHead>
+            <TableHead>Planned</TableHead>
+            <TableHead>Target</TableHead>
         </TableRow>
       {filteredIncome.length >0 ? (
       filteredIncome.map((income)=>(
        <TableRow key={income.id}>
-       <TableData>{income.parent}</TableData>
+        
+       <TableData><RemoveSymbol/>{income.parent.charAt(0).toUpperCase()+income.parent.substring(1)}</TableData>
       
-       <TableData>{income.amount}</TableData>
+       <TableData>{formatCurrency(income.amount)}</TableData>
+      
+       <TableData>{formatCurrency(income.target)}</TableData>
         </TableRow>
+
    ))):(<div>No Transaction selected for this date</div>)}
    </Table>
+   <AddSymbol/>
 </TableContainer>
 
 <TableContainer>
       <Table>
         <TableRow>
-            <TableHead>Income</TableHead>
+            <TableHead>Expense</TableHead>
             
-            <TableHead>Amount</TableHead>
+            <TableHead>Planned</TableHead>
+            <TableHead>Target</TableHead>
         </TableRow>
       {filteredTransactions.length >0 ?
       (filteredTransactions.map((transaction)=>(
         <TableRow key={transaction.id}>
-       <TableData>{transaction.parent}</TableData>
+          
+       <TableData><RemoveSymbol/>{transaction.parent.slice(0,1).toUpperCase()+transaction.parent.slice(1).toLowerCase()}</TableData>
       
-       <TableData>{transaction.amount}</TableData>
+       
+       <TableData>{formatCurrency(transaction.amount)}</TableData>
+       <TableData>{formatCurrency(transaction.target)}</TableData>
         </TableRow>
+        
+        
       ))):(<div>No Transaction selected for this date</div>)}
+    
     </Table>
+    <AddSymbol/>
     </TableContainer>
       </MyMiddleComponent>
       <RightComponent name1="Right"/>
--- src/routes/budget/myBudget.styles.jsx
+++ src/routes/budget/myBudget.styles.jsx
@@ -1,6 +1,37 @@
 import styled,{ createGlobalStyle }  from 'styled-components';
 import 'react-datepicker/dist/react-datepicker.css';
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { CiCircleRemove } from "react-icons/ci";
 
+export const TableData = styled.td`
+font-size:15px;
+// padding: 10px;
+  position: relative; // Ensure positioning context for RemoveSymbol
+
+`
+
+export const RemoveSymbol = styled(CiCircleRemove)`
+
+visibility: hidden;
+  cursor: pointer;
+
+  // Show the icon on hover of the TableData component
+  ${TableData}:hover & {
+    visibility: visible;
+    color: red;
+  }
+
+`
+export const AddSymbol= styled(IoIosAddCircleOutline)`
+font-size:20px;
+margin-left: 6px;
+margin-top:2px;
+
+&:hover{
+    color: green;
+    cursor: pointer;
+}
+`
 //createGlobalStyle from styled-components is used to define global styles that target the react-datepicker input elements.
 export const GlobalStyle= createGlobalStyle`
 .react-datepicker__input-container input {
@@ -28,46 +59,52 @@ width: 100%;
 height: 540px;
 display: flex;
 flex-direction: column;
-justify-content: center;
+// justify-content: center;
+
 
 `
 export const Table = styled.table`
-width:400px;
-margin-left:20px;
-margin-right:2px;
-margin-top: 4%
+width:370px;
+margin-left:1px;
+margin-right:1px;
+margin-top: 1%
 
 `
 export const DatePickerContainer=styled.div`
 margin-left:6px;
-width: 
+
 `
 export const TableRow = styled.tr`
 display: flex;
+padding: 5px;
 justify-content: space-between;
 &:first-child {
     background-color:#EEF7FF;
 }
-
+&:nth-child(n+2){
+    border-bottom: 1px solid #ddd;
+}
+margin-bottom: 5px;
 `
 
 export const TableHead = styled.th`
-
+height:20px;
 color: #686D76;
   text-align: left;
 `
-export const TableData = styled.td`
 
-`
 export const TableContainer=styled.div`
-width:500px;
+width:395px;
 // max-width: 200px;
 overflow-x: auto;
 border-radius: 8px;
-padding: 20px;
+padding: 8px;
  background-color:white;
-height:200px;
+// height:200px;
 margin-top:30px;
+margin-left: 170px;
 
-
+`
+export const HorizontalRule=styled.hr`
+display:flex;
 `
