57975ac5d0f1199f4e00fafee0853b1c914765f2
Stephen Afari <demorganafari@yahoo.com>
2024-07-27 15:43:40 +0000

    act vs bugd. page

--- src/routes/account/myAccount.js
+++ src/routes/account/myAccount.js
@@ -1,23 +1,121 @@
 import { SplitScreen } from "../../components/splitScreen/splitScreen";
 import { IoReorderFourSharp } from "react-icons/io5";
-import { AccountHeader,AccountIconContainer } from "./myAccount.styles";
+import { AccountHeader,AccountIconContainer, Dropdown, DropdownContainer, MonthDropdown, MyExpenseTable, MyIncomeTable, MyTable, TableContainer, TableRow, YearDropdown } from "./myAccount.styles";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { selectActualincomes } from "../../store/actualIncome/actualIncome.selector";
+import { selectActualtransactions } from "../../store/actualTransactions/actualTransactions.selector";
 
-const MiddleComponent=({name})=>{
-    return(<p style={{backgroundColor: 'yellow'}}>{name}</p>)
-  }
-  
-  const RightComponent=({name1})=>{
-    return(<p style={{backgroundColor: 'green'}}>{name1}</p>)
-  }
-  
+const years = [2022, 2023, 2024,2025,2026,2027,2028,2029,2030];
+const months = ['All', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  //This is the Budget vs Actual Page
   export const MyAccountScreen=()=>{
+    const dispatch = useDispatch();
+    const [selectedYear, setSelectedYear]= useState(new Date().getFullYear());
+    const [selectedMonth, setSelectedMonth]= useState(new Date().getMonth());
+    
+    const myActualIncomes = useSelector(selectActualincomes) || [];
+    const myActualExpenses = useSelector(selectActualtransactions) || [];
+
+    //Function to filter date by year and month
+    const filterDataByYearAndMonth = (data, year, month)=>{
+      return data.filter((item)=>{
+      let itemDate = new Date(item.date);
+      let itemYear = itemDate.getFullYear();
+      let itemMonth = itemDate.getMonth();
+
+      if(itemYear !== year) return false;
+      if(itemMonth==='All') return true;
+
+      return itemMonth === months.indexOf(month)-1; //indexOf(month) returns the index of the month string within the months array. For example, months.indexOf('January') returns 1.months.indexOf(month) - 1 converts the one-based index (starting from 1) to a zero-based index (starting from 0) to match the zero-based index returned by getMonth()
+
+    })
+    }
+const filteredIncomes = filterDataByYearAndMonth(myActualIncomes,selectedYear, selectedMonth);
+const filteredTransactions = filterDataByYearAndMonth(myActualExpenses,selectedYear, selectedMonth);
+console.log(filteredTransactions)
     return(
       <>
      <AccountHeader><AccountIconContainer><IoReorderFourSharp /> </AccountIconContainer>Budget vs Actual</AccountHeader>
-      {/* <SplitScreen  middleWeight={1} rightWeight={1}>
-      <MiddleComponent name="Middle"/>
-      <RightComponent name1="Right"/>
-        </SplitScreen> */}
+      <DropdownContainer>
+        <YearDropdown value={selectedYear} onChange={(e)=>setSelectedYear(parseInt(e.target.value))}>
+        {years.map((year)=>(
+          <option key={year} value={year}>{year}</option>
+        ))}
+        </YearDropdown>
+        <MonthDropdown value={selectedMonth} onChange={(e)=>setSelectedMonth(e.target.value)}>
+       {months.map((month)=>(
+        <option key={month} value={month}>{month}</option>
+       ))}
+        </MonthDropdown>
+        </DropdownContainer>
+        <TableContainer>
+       
+        <MyIncomeTable>
+          <thead>
+            <TableRow>
+              <th>Income</th>
+              <th>Plan</th>
+              <th>Actual</th>
+              <th>Var</th>
+              <th>Plan %</th>
+              <th>Actual %</th>
+              <th>Var %</th>
+            </TableRow>
+          </thead>
+          <tbody>
+            {filteredIncomes.length > 0 ? (
+              filteredIncomes.map((income) => (
+                <tr key={income.id}>
+                  <td>{new Date(income.date).toLocaleDateString()}</td>
+                  <td>{income.description}</td>
+                  <td>{income.amount}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="3">No incomes for the selected period</td>
+              </tr>
+            )}
+          </tbody>
+        </MyIncomeTable>
+
+       
+        <MyExpenseTable>
+        <thead>
+            <TableRow>
+              <th>Expense</th>
+              <th>Plan</th>
+              <th>Actual</th>
+              <th>Var</th>
+              <th>Plan %</th>
+              <th>Actual %</th>
+              <th>Var %</th>
+            </TableRow>
+          </thead>
+          <tbody>
+            {filteredTransactions.length > 0 ? (
+              filteredTransactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td>{new Date(transaction.date).toLocaleDateString()}</td>
+                  <td>{transaction.description}</td>
+                  <td>{transaction.amount}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="3">No transactions for the selected period</td>
+              </tr>
+            )}
+          </tbody>
+        </MyExpenseTable>
+      </TableContainer>
+
+
+   
+
+      
         </>
     )
   }
--- src/routes/account/myAccount.styles.jsx
+++ src/routes/account/myAccount.styles.jsx
@@ -7,3 +7,65 @@ margin-top: 2px;
 export const AccountIconContainer = styled.div`
 margin-top: 2px;
 `
+export const DropdownContainer = styled.div`
+  display: flex;
+flex-direction: column;
+  margin-bottom: 20px;
+  height: 50px;
+  gap: 5px;
+  margin-left: 3px;
+`
+export const YearDropdown = styled.select`
+  padding: 5px;
+  margin-right: 10px;
+  width:6%;
+ border: none;
+ Placeholder:'yyy'
+`;
+export const MonthDropdown = styled.select`
+  padding: 5px;
+  margin-right: 10px;
+  width:9%;
+  border: none;
+ 
+`;
+
+export const TableRow =styled.tr`
+display: flex;
+padding: 5px;
+justify-content: space-between;
+&:first-child {
+    background-color:#EEF7FF;
+}
+//from the second child onwards (ie. n starts from zero)
+&:nth-child(n+2){
+    border-bottom: 1px solid #ddd;
+}
+margin-bottom: 5px;
+width: 100%
+
+`
+
+export const TableContainer = styled.div`
+  margin-top: 20px;
+  width:2500px;
+  margin-left:10px;
+`;
+
+export const TableHead = styled.thead`
+ width:100%
+
+`
+export const MyIncomeTable= styled.table`
+width: 100%;
+border-collapse: collapse;
+table-layout: fixed;  //fixed table layout
+font-family: 'Courier New', Courier, monospace; /* Monospaced font */
+`
+
+export const MyExpenseTable= styled.table`
+width: 100%;
+border-collapse: collapse;
+table-layout: fixed;  //fixed table layout
+font-family: 'Courier New', Courier, monospace; /* Monospaced font */
+`
