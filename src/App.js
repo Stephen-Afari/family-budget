bd9594f9336fb77feaa8f8ed914f029d6d739028
Stephen Afari <demorganafari@yahoo.com>
2024-12-26 06:18:18 +0000

    update

--- src/App.js
+++ src/App.js
@@ -17,14 +17,37 @@ import { BudgetIncomeFetcher } from "./components/common/fetchBudgetIncome";
 import { ActualTransactionFetcher } from "./components/common/fetchActualTransaction";
 import { BudgetTransactionFetcher } from "./components/common/fetchBudgetTransaction";
 import { ActualIncomeFetcher } from "./components/common/fetchActualIncome";
+import { useEffect, useState } from "react";
 
 //React Query requires you to wrap your app with a QueryClientProvider, which provides the QueryClient to your React components. This is necessary to manage and configure queries globally within your application.
 // Create a client
 const queryClient = new QueryClient();
 
 // PrivateRoute component.This will guard routes that require authentication.
+/**
+ * PrivateRoute component:
+ * Protects routes that require authentication by checking the token in Redux.
+ * If the token is not available, redirects the user to the login page.
+ */
 const PrivateRoute = ({ children }) => {
-  const token = useSelector(selectUserToken); // Select the token from Redux
+  const token = useSelector(selectUserToken); // Fetch token from Redux store
+  const [isReady, setIsReady] = useState(false); // Local state to track readiness
+
+  useEffect(() => {
+    // Simulate readiness check when token becomes available
+    if (token) {
+      setIsReady(true);
+    } else {
+      setIsReady(false);
+    }
+  }, [token]);
+
+  // Show a loading spinner while checking authentication
+  if (!isReady) {
+    return <div>Loading...</div>;
+  }
+
+  // If token exists, render protected content; otherwise, redirect to login
   return token ? children : <Navigate to="/logIn" />;
 };
 
@@ -43,6 +66,7 @@ function App() {
    <ActualTransactionFetcher/>
     <BudgetIncomeFetcher/>  
     <Navigation /></PrivateRoute>} >
+
 <Route path='budget' element={<MyBudgetScreen/>}/>
 <Route path='transactions' element={<MyTransactionScreen/>}/>
 <Route path='account' element={<MyAccountScreen/>}/>
@@ -52,6 +76,8 @@ function App() {
 <Route path='export' element={<MyExportScreen/>}/>
 
   </Route>
+{/* Redirect unmatched routes to the login page */}
+<Route path="*" element={<Navigate to="/logIn" />} />
 </Routes>
 </QueryClientProvider>
   );
